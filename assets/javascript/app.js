e9bfa644587441bdd0a120f3ce1cdc5e6588e3ff
ANDRES <andres@ANDRESs-MacBook-Air.local>
2019-10-23 23:26:51 -0500

    files updated

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+const startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click,startGame')
+
+    
+
+
+
+
+
+function startGame(){
+    console.log('started')
+
+
+}
+
+function question(){
+
+
+}
+function selectAnswer(){
+
+
+
+}
